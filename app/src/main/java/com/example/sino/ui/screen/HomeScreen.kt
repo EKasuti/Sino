ea5443fce435dd66baf137bc82ddbaf3e6196b14
Emmanuel Kasuti Makau <emmanuel.k.makau.jr.26@dartmouth.edu>
2025-12-10 14:19:25 +0100

    moves wellness scorecard to components folder

--- app/src/main/java/com/example/sino/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/sino/ui/screen/HomeScreen.kt
@@ -3,28 +3,19 @@ package com.example.sino.ui.screen
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.sino.R
 import com.example.sino.ui.components.WellnessScoreCard
-import com.example.sino.ui.theme.Balanced
-import com.example.sino.ui.theme.Excellent
-import com.example.sino.ui.theme.Relaxed
-import com.example.sino.ui.theme.Stressed
-import com.example.sino.ui.theme.Tense
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
